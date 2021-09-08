43199de4e19caaeefb08ed205e2bcb29a647e738
LupusWhiteFox <observative2oppp@gmail.com>
2021-09-08 17:33:51 +0700

    Add project files.

---
+++ mvc/Controllers/BookslistController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using mvc.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc.Controllers
+{
+    public class BookslistController : Controller
+    {
+        private readonly Context _db;
+
+        [BindProperty]
+        public Book Books { get; set; }
+
+        public BookslistController(Context db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            Books = new Book();
+            if (id == null)
+            {
+                return View(Books);
+            }
+            Books = _db.Books.FirstOrDefault(u=>u.Id == id);
+            if(Books == null)
+            {
+                return NotFound();
+            }
+            return View(Books);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert()
+        {
+            if (ModelState.IsValid)
+            {
+                if (Books.Id == 0)
+                {
+                    _db.Books.Add(Books);
+                    
+                }
+                else
+                {
+                    _db.Books.Update(Books);
+                    
+                }
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(Books);
+        }
+
+
+        #region API Calls
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Json(new { data = await _db.Books.ToListAsync() });
+        }
+
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var bookFromDb = await _db.Books.FirstOrDefaultAsync(u => u.Id == id);
+            if (bookFromDb == null)
+            {
+                return Json(new { success = false, message = "Erorr" });
+            }
+
+            _db.Books.Remove(bookFromDb);
+            await _db.SaveChangesAsync();
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+
+        #endregion
+    }
+}
---
+++ mvc/Startup.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Pomelo.EntityFrameworkCore.MySql;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using mvc.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var serverVersion = new MariaDbServerVersion(new Version(10, 5, 11));
+
+            services.AddDbContext<Context>(options => options.UseMySql(Configuration.GetConnectionString("DefaultConnection"), serverVersion));
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
